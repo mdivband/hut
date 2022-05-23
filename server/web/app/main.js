bd74f3af3e51ae6d7a30f8a0e810ad91167d2f41
Ashley Pare <agp1g13@soton.ac.uk>
2022-05-23 22:44:03 +0100

    Added tutorial video and video survey

--- server/web/app/main.js
+++ server/web/app/main.js
@@ -363,8 +363,28 @@ var simulator = {
                             self.surveyDone = true;
                             $.post("/mode/scenario/closeSurvey", {}, function () {
                                 $("#overlay_div").empty();
-                                $("#overlay_div").hide();
-                                _.bind(self.run, self)();
+                                var closeVideo = $('<button id="close_video" style="cursor: pointer;">Close Video</button>').appendTo($("#overlay_div"));
+                                $('<br>').appendTo($("#overlay_div"));
+                                var videoFrame = $('<iframe width="90%" height="90%" src="https://www.youtube.com/embed/se0vuA1uVmk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>').appendTo($("#overlay_div"));
+                                closeVideo.on('click', function () {
+                                    var isSure = confirm("Have you watched the tutorial video?");
+                                    if (isSure) {
+                                        $("#overlay_div").empty();
+                                        var closeVideoSurvey = $('<button id="close_survey" style="cursor: pointer;">Close Survey</button>').appendTo($("#overlay_div"));
+                                        $('<br>').appendTo($("#overlay_div"));
+                                        var videoSurvey = "https://forms.office.com/Pages/ResponsePage.aspx?id=-XhTSvQpPk2-iWadA62p2CmPPgx944RCrlRRT-uovIBUREpUOTMxMFJHV1dNQ1RPTDhFVkJNOVJTSy4u&embed=true";
+                                        var surveySource = videoSurvey;
+                                        var videoSurveyFrame = $('<iframe width="40%" height= "90%" src=' + surveySource + ' frameborder= "0" marginwidth= "0" marginheight= "0" style= "border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>').appendTo($("#overlay_div"));
+                                        closeVideoSurvey.on('click', function () {
+                                            var isSure = confirm("Have you completed and submitted the survey?");
+                                            if (isSure) {
+                                                $("#overlay_div").empty();
+                                                $("#overlay_div").hide();
+                                                _.bind(self.run, self)();
+                                            }
+                                        });
+                                    }
+                                });
                             });
                         }
                     });
