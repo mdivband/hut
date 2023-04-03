bb9733f123bc03154c4cfcd3e0471dec56e05062
yashbrid13 <yash.brid@fafadiatech.com>
2023-04-03 11:13:10 +0530

    Added tests

---
+++ server/src/test/java/server/SimulatorTest.java
@@ -0,0 +1,29 @@
+
+package server;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class SimulatorTest {
+
+	// @Test
+	// @DisplayName("Should Return True")
+	// void addsTwoNumbers() {
+	// 	Simulator sim = new Simulator();
+	// 	assertEquals(true, sim.loadScenarioMode("debrisTest.json"), "1 + 1 should equal 2");
+	// }
+
+	// @Test
+	// @DisplayName("Should add")
+	// void addsTwoNumber()
+	// {
+	// 	Simulator sim = new Simulator();
+	// 	assertEquals(2, sim.add(1, 1), "1+1 should equal 2");
+	// }
+}
