4f4eef634671d8c09a48b075e87e1a25fa0deb68
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2025-03-06 12:20:14 +0000

    added risk map file checks

--- server/src/server/controller/RiskMapController.java
+++ server/src/server/controller/RiskMapController.java
@@ -21,31 +21,33 @@ public class RiskMapController extends AbstractController{
         // TODO: A 2D array is fine for now, but unclear
         ArrayList<HashMap<String, ArrayList<Double>>> riskHeatMap = new ArrayList<HashMap<String, ArrayList<Double>>>();
 
-        try {
-            String json = GsonUtils.readFile(this.riskFileName);
-            Object obj = GsonUtils.fromJson(json);
-            ArrayList features = GsonUtils.getValue(obj, "features");
+        if(this.riskFileName != null) {
+            try {
+                String json = GsonUtils.readFile(this.riskFileName);
+                Object obj = GsonUtils.fromJson(json);
+                ArrayList features = GsonUtils.getValue(obj, "features");
 
-            for(Object loc : features){
-                Object properties = GsonUtils.getValue(loc, "properties");
-                if(!GsonUtils.hasKey(properties, "FIRE")) continue;
+                for(Object loc : features){
+                    Object properties = GsonUtils.getValue(loc, "properties");
+                    if(!GsonUtils.hasKey(properties, "FIRE")) continue;
 
-                Double left = GsonUtils.getValue(properties, "left");
-                Double right = GsonUtils.getValue(properties, "right");
-                Double top = GsonUtils.getValue(properties, "top");
-                Double bottom = GsonUtils.getValue(properties, "bottom");
-                Double fire = GsonUtils.getValue(properties, "FIRE");
+                    Double left = GsonUtils.getValue(properties, "left");
+                    Double right = GsonUtils.getValue(properties, "right");
+                    Double top = GsonUtils.getValue(properties, "top");
+                    Double bottom = GsonUtils.getValue(properties, "bottom");
+                    Double fire = GsonUtils.getValue(properties, "FIRE");
 
-                HashMap<String, ArrayList<Double>> newPlace = new HashMap<String, ArrayList<Double>>();
+                    HashMap<String, ArrayList<Double>> newPlace = new HashMap<String, ArrayList<Double>>();
 
-                // average LR and TB to get center
-                newPlace.put("coordinates", new ArrayList<Double>(Arrays.asList(0.5*(left+right),0.5*(top+bottom))));
-                newPlace.put("FIRE", new ArrayList<Double>(Arrays.asList(fire)));
-                riskHeatMap.add(newPlace);
+                    // average LR and TB to get center
+                    newPlace.put("coordinates", new ArrayList<Double>(Arrays.asList(0.5*(left+right),0.5*(top+bottom))));
+                    newPlace.put("FIRE", new ArrayList<Double>(Arrays.asList(fire)));
+                    riskHeatMap.add(newPlace);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         simulator.getState().setRiskMap(riskHeatMap);
@@ -61,45 +63,47 @@ public class RiskMapController extends AbstractController{
 
         HashMap<String,Double> features = simulator.getState().getRiskMapWeights();
 
-        try {
-            String json = GsonUtils.readFile(this.riskFileName);
-            Object obj = GsonUtils.fromJson(json);
-            ArrayList attributes = GsonUtils.getValue(obj, "features");
+        if(this.riskFileName != null) {
+            try {
+                String json = GsonUtils.readFile(this.riskFileName);
+                Object obj = GsonUtils.fromJson(json);
+                ArrayList attributes = GsonUtils.getValue(obj, "features");
 
-            for(Object loc : attributes){
-                Object properties = GsonUtils.getValue(loc, "properties");
-                Object geometry = GsonUtils.getValue(loc, "geometry");
+                for(Object loc : attributes){
+                    Object properties = GsonUtils.getValue(loc, "properties");
+                    Object geometry = GsonUtils.getValue(loc, "geometry");
 
-                boolean skip = false;
-                for(String key : features.keySet()){
-                    if(!GsonUtils.hasKey(properties, key)) {
-                        skip = true;
-                        break;
+                    boolean skip = false;
+                    for(String key : features.keySet()){
+                        if(!GsonUtils.hasKey(properties, key)) {
+                            skip = true;
+                            break;
+                        }
                     }
+                    if(skip) continue;
+
+
+
+                    // annoyingly wrapped in a single value array
+                    ArrayList<ArrayList<ArrayList<Double>>> coordinates = GsonUtils.getValue(geometry, "coordinates");
+
+                    HashMap<String, ArrayList<Double>> newPlace = new HashMap<String, ArrayList<Double>>();
+                    newPlace.put("coordinates", new ArrayList<Double>());
+
+                    for(ArrayList<Double> point : coordinates.get(0)){
+                        newPlace.get("coordinates").add(point.get(0));
+                        newPlace.get("coordinates").add(point.get(1));
+                    }
+                    for(String feature : features.keySet()){
+                        Double value = GsonUtils.getValue(properties, feature);
+                        newPlace.put(feature, new ArrayList<Double>(Arrays.asList(value)));
+                    }
+                    riskHeatMap.add(newPlace);
                 }
-                if(skip) continue;
 
-
-
-                // annoyingly wrapped in a single value array
-                ArrayList<ArrayList<ArrayList<Double>>> coordinates = GsonUtils.getValue(geometry, "coordinates");
-
-                HashMap<String, ArrayList<Double>> newPlace = new HashMap<String, ArrayList<Double>>();
-                newPlace.put("coordinates", new ArrayList<Double>());
-
-                for(ArrayList<Double> point : coordinates.get(0)){
-                    newPlace.get("coordinates").add(point.get(0));
-                    newPlace.get("coordinates").add(point.get(1));
-                }
-                for(String feature : features.keySet()){
-                    Double value = GsonUtils.getValue(properties, feature);
-                    newPlace.put(feature, new ArrayList<Double>(Arrays.asList(value)));
-                }
-                riskHeatMap.add(newPlace);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         simulator.getState().setRiskMap(riskHeatMap);
