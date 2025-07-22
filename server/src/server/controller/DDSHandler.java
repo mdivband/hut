7e9187f3329087b538068173a6879d3240196dba
William Hunt <w.hunt@soton.co.uk>
2025-07-22 12:44:39 +0100

    (fixed prev commit)

--- server/src/server/controller/DDSHandler.java
+++ server/src/server/controller/DDSHandler.java
@@ -6,14 +6,15 @@ public class DDSHandler {
     private State state;
 
     public DDSHandler(State state) {
+        this.state = state;
+    }
+
+    public void update() {
+        System.out.println("Here we pull from DDS");
         // At each timestep this is called. We should update everything that has changed
 
 
         double heading = 0.0;  // Would actually be retrieved from DDS
         state.getAgent("UAV-1").setHeading(heading);
-    }
-
-    public void update() {
-        System.out.println("Here we pull from DDS");
     }
 }
